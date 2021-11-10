df022e6261a86a008575caecfe0e094d2c97e2c2
Guilherme <guilhaugusto05@gmail.com>
2021-11-10 17:42:59 -0300

    Exercícios 2 e 3

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import PokemonDetails from './PokemonDetails';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route
-          path="/"
-          render={(props) =>
-            <Pokedex { ...props } pokemons={pokemons} />
-          }
-        />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact path="/"
+            render={(props) =>
+              <Pokedex { ...props } pokemons={pokemons} />
+            }
+          />
+          <Route
+            exact path="/pokemon/:id"
+            render={(props) =>
+              <PokemonDetails { ...props } />
+            }
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/PokemonDetails.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import pokemons from './data';
+
+class PokemonDetails extends Component {
+  render() {
+    const { id } = this.props.match.params;
+
+    const pokemon = pokemons.find((poke) => poke.id === parseInt(id));
+    const {
+      name,
+      type,
+      averageWeight: { value, measurementUnit },
+      image,
+      moreInfo,
+      foundAt,
+    } = pokemon;
+
+    return (
+      <div className="pokemon-details">
+        <img
+          src={ image }
+          alt={ name }
+          className="pokemon-img"
+        />
+        <p className="pokemon-info">
+          <span>{ name }</span>
+          <span> | </span>
+          <span>{ type }</span>
+          <span> | </span>
+          <span>{ `${value} ${measurementUnit}` }</span>
+        </p>
+        <p className="pokemon-more-info">
+          <a
+            href={ moreInfo }
+            rel="noopener nofollow noreferrer"
+            target="_blank"
+          >
+            Click here for more info
+          </a>
+        </p>
+        <div className="pokemon-maps">
+          {foundAt.map((place, index) => {
+            const { location, map } = place;
+            return (
+              <div className="pokemon-map" key={ `map${index}` }>
+                <p>{ location }</p>
+                <img src={ map } alt={ location } />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PokemonDetails;
